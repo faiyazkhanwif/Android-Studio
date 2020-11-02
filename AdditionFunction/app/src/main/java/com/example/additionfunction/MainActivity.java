2cccbfc4072c17e16d7c4ff772f9dc78474d103f
faiyazkhanwif <faiyazkhanwif@gmail.com>
2020-11-03 01:07:19 +0800

    Function added

--- AdditionFunction/app/src/main/java/com/example/additionfunction/MainActivity.java
+++ AdditionFunction/app/src/main/java/com/example/additionfunction/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.additionfunction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Addition(View view){
+        EditText etnum1 = (EditText)findViewById(R.id.et1);
+        EditText etnum2 = (EditText)findViewById(R.id.et2);
+        TextView tvres = (TextView)findViewById(R.id.tv2);
+        TextView tvheadres = (TextView)findViewById(R.id.tv3);
+        tvheadres.setText("Result: ");
+        int result = Integer.parseInt(etnum1.getText().toString()) + Integer.parseInt(etnum2.getText().toString());
+        tvres.setText(Integer.toString(result));
+
     }
 }
