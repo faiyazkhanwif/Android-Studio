79bdca345e47f8f7c0f1731289474c0a0f7b1fcc
faiyazkhanwif <faiyazkhanwif@gmail.com>
2020-11-18 09:03:45 +0800

    Calculator functionality added

--- MultipleActivitiesAndLayout/app/src/main/java/com/example/multipleactivitiesandlayout/GridActivity.java
+++ MultipleActivitiesAndLayout/app/src/main/java/com/example/multipleactivitiesandlayout/GridActivity.java
@@ -2,11 +2,15 @@ package com.example.multipleactivitiesandlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 public class GridActivity extends AppCompatActivity {
     Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnadd,btnmin,btnmul,btndiv,btndot,btnc,btneq;
@@ -28,14 +32,154 @@ public class GridActivity extends AppCompatActivity {
         btn7 = findViewById(R.id.sevenbtn);
         btn8 = findViewById(R.id.eightbtn);
         btn9 = findViewById(R.id.ninebtn);
+
         btnadd = findViewById(R.id.addbtn);
         btnmin = findViewById(R.id.minbtn);
         btnmul = findViewById(R.id.multbtn);
         btndiv = findViewById(R.id.dvbtn);
+
         btndot = findViewById(R.id.dotbtn);
         btnc = findViewById(R.id.cbtn);
         btneq = findViewById(R.id.eqbtn);
-        
+
+        tvinp = findViewById(R.id.inpshow);
+        tvout = findViewById(R.id.ans);
+
+        btnc.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                tvinp.setText("");
+                tvout.setText("");
+            }
+        });
+        btn0.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"0");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"9");
+            }
+        });
+
+        btnadd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"+");
+            }
+        });
+        btnmin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"−");
+            }
+        });
+        btnmul.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"×");
+            }
+        });
+        btndot.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+".");
+            }
+        });
+        btndiv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                tvinp.setText(process+"÷");
+            }
+        });
+
+        btneq.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                process = tvinp.getText().toString();
+                process = process.replaceAll("×","*");
+                process = process.replaceAll("÷","/");
+                Context rhino = Context.enter();
+                rhino.setOptimizationLevel(-1);
+                String finalresult = "";
+                try {
+                    Scriptable scriptable = rhino.initStandardObjects();
+                    finalresult = rhino.evaluateString(scriptable,process,"javascript",1,null).toString();
+                }catch (Exception e){
+                    finalresult="0";
+                }
+
+                tvout.setText(finalresult);
+            }
+        });
+
+
     }
     public void GRDBackClicked(View view){
         finish();
